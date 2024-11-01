7436a05ae35e027b4f8bcab55be0196759b8ec4b
Anton Spaskyi <anton.spaskyi@onevisionresources.com>
2024-11-01 18:41:55 +0100

    initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { WordListComponent } from './word-list/word-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: WordListComponent }];
---
+++ src/app/word-list/word-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './word-list.component.html',
+  styleUrl: './word-list.component.css'
+})
+export class WordListComponent {
+
+}
