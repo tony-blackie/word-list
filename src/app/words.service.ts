4793e55c2180c3f92002dc96aae391776eecf624
Anton Spaskyi <anton.spaskyi@useo.pl>
2024-11-24 17:43:41 +0100

    fix api routes import

--- src/app/words.service.ts
+++ src/app/words.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { API_ROUTES } from '../constants';
 
 export interface Word {
   text: string;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
-const API_URL = 'http://localhost:3000';
+export const API_URL = 'http://localhost:3000';
 
-const API_ROUTES = {
+export const API_ROUTES = {
   USERS: `${API_URL}/users`,
   WORDS: `${API_URL}/words`,
 };
